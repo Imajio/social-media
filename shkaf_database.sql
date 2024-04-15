7714b560e020bf9daa6c3982f22904e7ac100c56
Imajio <inkognito.gomel@gmail.com>
2024-04-15 14:50:00 +0200

    DataBase Structure For Import ))

--- shkaf_database.sql
+++ shkaf_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Мар 31 2024 г., 01:17
+-- Время создания: Апр 15 2024 г., 14:48
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -38,10 +38,43 @@ CREATE TABLE `chats` (
 --
 
 INSERT INTO `chats` (`IdChat`, `NickFirst`, `NickSecond`) VALUES
-(44, 'test', 'test2'),
-(45, 'mooni', 'test'),
-(46, 'mooni', 'nickname'),
-(47, 'nickname', 'test2');
+(57, 'test', 'test2'),
+(58, 'hujnia', 'mooni'),
+(59, 'test', 'test3'),
+(60, 'test', 'test4');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE `messages` (
+  `Message_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `message_text` varchar(255) DEFAULT NULL,
+  `Timestamp` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `messages`
+--
+
+INSERT INTO `messages` (`Message_id`, `sender_id`, `receiver_id`, `message_text`, `Timestamp`) VALUES
+(2, 36, 38, 'hi!!!!', '2024-04-04 12:55:52'),
+(4, 52, 36, 'hello', '2024-04-05 14:44:11'),
+(5, 36, 52, 'dspijfsdkfp[sdfsdfsdfdsf', '2024-04-05 15:09:13'),
+(6, 36, 52, 'dsgfdhghkhjlk;lk\'l;gfhgjkhlj;', '2024-04-05 15:28:56'),
+(7, 36, 52, 'sdfgdfhjkl;\';ljkhgfdsa', '2024-04-05 15:28:58'),
+(8, 36, 52, 'fdsdghjkl;\';kjhgfdsa', '2024-04-05 15:29:02'),
+(9, 36, 52, 'asdfghjkl;\'lkjhgfdsafghjkl;\'lkjhgfds', '2024-04-05 15:29:05'),
+(10, 36, 52, 'dsasfghjkl;jhgfdsafdgfhjkl;jhgfds', '2024-04-05 15:29:07'),
+(11, 36, 52, 'sdafdghkjl;lkjhgfdsfgfhkjl;jhgfdfgdhjj', '2024-04-05 15:29:11'),
+(12, 36, 47, 'htllo', '2024-04-05 16:46:05'),
+(13, 36, 47, 'sadasdasda', '2024-04-06 21:34:00'),
+(20, 36, 47, '', '2024-04-15 14:36:03'),
+(21, 36, 47, 'щштфызьхвлфьхщвазыьх123', '2024-04-15 14:40:57');
 
 -- --------------------------------------------------------
 
@@ -64,13 +97,18 @@ INSERT INTO `users` (`Id`, `Login`, `Password`) VALUES
 (38, 'mooni', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
 (39, 'nonenick', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
 (40, 'nickname', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
-(41, 'huju', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
 (42, 'kopl', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
 (43, 'anonim', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
 (44, 'test3', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
 (45, 'test4', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
 (46, 'olga', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
-(47, 'test2', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c');
+(47, 'test2', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
+(48, '3ef3f3fqerf3', '048a9e5b570aec14b63efd0b0a5cbc7b39949fd2e41c052c5e4d1e4902c0144e'),
+(49, 'test5', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
+(50, 'test6', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
+(51, 'test7', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
+(52, 'danik', '7b3d979ca8330a94fa7e9e1b466d8b99e0bcdea1ec90596c0dcc8d7ef6b4300c'),
+(53, 'hujnia', '551d83276d3689d7056024d0f29d6fd44681c51149d0076c4066c41dc7ac34c4');
 
 --
 -- Индексы сохранённых таблиц
@@ -81,6 +119,14 @@ INSERT INTO `users` (`Id`, `Login`, `Password`) VALUES
 --
 ALTER TABLE `chats`
   ADD PRIMARY KEY (`IdChat`);
+
+--
+-- Индексы таблицы `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`Message_id`),
+  ADD KEY `sender_id` (`sender_id`),
+  ADD KEY `receiver_id` (`receiver_id`);
 
 --
 -- Индексы таблицы `users`
@@ -96,13 +142,30 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `chats`
 --
 ALTER TABLE `chats`
-  MODIFY `IdChat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `IdChat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT для таблицы `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `Message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`Id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
